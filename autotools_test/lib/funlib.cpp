b3db9b356ed45588e546d5e325df22bd67a4cc2c
Sreeram.la <lasreeram@gmail.com>
2016-08-20 05:15:21 -0400

    test auto tools in preparation for changing make

---
+++ autotools_test/lib/funlib.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <libfun.h>
+void fun(){
+	std::cout << "this is fun" << std::endl;
+	std::cout << "and good for health" << std::endl;
+}
---
+++ autotools_test/src/hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <libfun.h>
+
+int main(){
+	std::cout << "hello world" << std::endl;
+	fun();
+}
